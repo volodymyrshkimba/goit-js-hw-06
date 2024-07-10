bad2aa16d2e5189f00a588a22e9a3674847d83dd
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-07-11 00:38:26 +0300

    first check

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,3 @@
-// Склад
-
-// Створи клас Storage, який створюватиме об'єкти для управління складом товарів. Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного об'єкта в приватну властивість items.
-
-// Оголоси наступні методи класу:
-
-// getItems() — повертає масив поточних товарів у приватній властивості items.
-// addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
-// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
-
-
-// Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
-
 class Storage {
 	#items;
 
@@ -23,13 +10,18 @@ class Storage {
 	addItem(newItem) {
 		this.#items.push(newItem);
 	}
-	removeitem(itemToRemove) {
-		this.#items.splice()
+	removeItem(itemToRemove) {
+		if (this.#items.includes(itemToRemove)) {
+			this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+		}
 	}
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
