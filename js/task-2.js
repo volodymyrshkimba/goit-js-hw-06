45c3ce4fb2e0e1d03c0e9910ec8912821f09bfe3
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-07-10 16:55:42 +0300

    start doing

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,35 @@
-function getShippingMessage(country, price, deliveryFee) {
-	return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+// Склад
+
+// Створи клас Storage, який створюватиме об'єкти для управління складом товарів. Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного об'єкта в приватну властивість items.
+
+// Оголоси наступні методи класу:
+
+// getItems() — повертає масив поточних товарів у приватній властивості items.
+// addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
+// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
+
+
+// Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+
+class Storage {
+	#items;
+
+	constructor(arr) {
+		this.#items = arr;
+	}
+	getItems() {
+		return this.#items;
+	}
+	addItem(newItem) {
+		this.#items.push(newItem);
+	}
+	removeitem(itemToRemove) {
+		this.#items.splice()
+	}
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
